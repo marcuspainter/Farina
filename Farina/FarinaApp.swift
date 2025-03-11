a9908d98c407fff418e6c4af7575c22f65a38dda
Marcus Painter <marcus@marcuspainter.com>
2025-03-11 16:11:05 +0000

    Initial Commit

---
+++ Farina/FarinaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FarinaApp.swift
+//  Farina
+//
+//  Created by Marcus Painter on 11/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct FarinaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
